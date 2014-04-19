7205f2671d0a1eb0d3255176237d16436fc8110c
deonoway <richard.c.crafton@gmail.com>
2014-04-19 18:52:18 -0400

    added html5 shiv

--- application/views/inc/head.php
+++ application/views/inc/head.php
@@ -1,5 +1,8 @@
 	<head>
 		<title><?php echo($title);?></title>
+		<!--[if lt IE 9]>
+		<script src="dist/html5shiv.js"></script>
+		<![endif]--> 
 		<link rel="stylesheet" type="text/css" href="<? echo(asset('css/main.css')); ?>">
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js">
 		</script>
