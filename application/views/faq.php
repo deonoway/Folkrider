9f58ad9f42c85f630ffdc2024190a73086e79282
deonoway <richard.c.crafton@gmail.com>
2014-04-26 22:38:27 -0400

    finished styling FAQ page

--- application/views/faq.php
+++ application/views/faq.php
@@ -12,29 +12,39 @@
 		?>
 		<div id="faq">
 			<div>
-				<div class="question"><span>Q:</span>Why Should I Join Folkrider?</div>
-				<div class="answer"><span>A:</span>Joining allows you to download all of the music the site will ever have for life for this one-time fee. At our current rate thats over 20 hours of MP3s by the end of the year. This makes Folkrider the easiest and least expensive way to support independent artists in the genre.</div>
-			</div>
-
-			<!--<div>
-				<p class="question"><span>Q:</span>Does the radio show change?</p>
-				<p class="answer"><span>A:</span>Yes. Beginning the 1st of October the show will be updated weekly. Several LIVE shows on a weekly basis are planned for the radio show beginning Jan 1, 2013. We are currently working on a featured artist show and a Tuesday night Live open mic, so join now and submit your stuff in the new link beginning November and get on Folkrider Radio !!!</p>
+				<div class="question"><div class="em">Q:</div><p class="em">Why Should I Join Folkrider?</p></div>
+				<div class="clear"></div>
+				<div class="answer"><div class="em">A:</div><p>Joining allows you to download all of the music we will ever have for life for the life of the site, and just for a one-time fee. At our current rate thats over 20 hours of MP3s by the end of the year. This makes Folkrider the easiest and least expensive way to support independent artists in the genre.</p></div>
+				<div class="clear"></div>
 			</div>
 
 			<div>
-				<p class="question"><span>Q:</span>How do I become a Folkrider Artist?</p>
-				<p class="answer"><span>A:</span>Once you join Folkrider you can go to the artist contact page and put your information in and 3 MP3s for review. If you are accepted you will be sent out agreement and fees, you supply your website, merchant account information, (we can help with these for you if you donâ€™t have them) and promotional materials. Thats it!</p>
+				<div class="question"><div class="em">Q:</div><p class="em">Does the radio show change?</p></div>
+				<div class="clear"></div>
+				<div class="answer"><div class="em">A:</div><p>Yes. Beginning the 1st of October the show will be updated weekly. Several LIVE shows on a weekly basis are planned for the radio show beginning Jan 1, 2015. We are currently working on a featured artist show and a Tuesday night Live open mic, so join now and submit your stuff!!!</p></div>
+				<div class="clear"></div>
+			</div>
+
+			<div>
+				<div class="question"><div class="em">Q:</div><p class="em">How do I become a Folkrider Artist?</p></div>
+				<div class="clear"></div>
+				<div class="answer"><div class="em">A:</div><p>Once you join Folkrider go to the contact page and put your information in for review. If you are accepted you will be sent out agreement and fees, you supply your website, merchant account information, (we can help with these for you if you don't have them) and promotional materials. Thats it!</p></div>
+				<div class="clear"></div>
 			</div>
 			
 			<div>
-				<p class="question"><span>Q:</span>How do I become a sponsor?</p>
-				<p class="answer"><span>A:</span>Go to the Sponsors contact link and send us your request. We will respond with rates and features.</p>
+				<div class="question"><div class="em">Q:</div><p class="em">How do I become a sponsor?</p></div>
+				<div class="clear"></div>
+				<div class="answer"><div class="em">A:</div><p>Go to the Contact and send us your request. We will respond with rates and features.</p></div>
+				<div class="clear"></div>
 			</div>
 
 			<div>
-				<p class="question"><span>Q:</span>What do I need to join Folkrider?</p>
-				<p class="answer"><span>A:</span>You must have a pulse and $12. We are sdivict on these requirements unlike some Elections Boards.</p>
-			</div>-->
+				<div class="question"><div class="em">Q:</div><p class="em">What do I need to join Folkrider?</p></div>
+				<div class="clear"></div>
+				<div class="answer"><div class="em">A:</div><p>You must have a pulse, and $12. We are sdivict on these requirements.</p></div>
+				<div class="clear"></div>
+			</div>
 		</div>
 		<?
 			$this->load->view('inc/footer');
